0ccd5c1802de470ba1a40cdbbeef96b81770cf5e
MrudulaPatil7192 <166419816+MrudulaPatil7192@users.noreply.github.com>
2026-07-21 14:43:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ HTML_LAYOUT = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>✨ Cute Dream House Estimator AI ✨</title>
+    <title>✨Dream House Estimator AI ✨</title>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <style>
         :root {
